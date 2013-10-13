0df75a9fa4bf2436cf5380e6dc1e532cdb7bdfe8
chry <chrywhy@gmail.com>
2013-10-13 22:03:01 +0800

    add 3 java

---
+++ serverInHome/src/main/java/com/serverinhome/gateServer/NewUser.java
@@ -0,0 +1,43 @@
+package com.serverinhome.gateServer;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/newUser")
+public class NewUser {
+
+    private static Set<Session> peers = Collections.newSetFromMap(new ConcurrentHashMap<Session, Boolean>());
+    private static String _text = "";
+
+    @OnOpen
+    public void onOpen(Session client) throws IOException, EncodeException {
+        System.out.print("################# open -" + _text);
+        peers.add(client);
+        client.getBasicRemote().sendText(_text);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        peers.remove(session);
+    }
+
+    @OnMessage
+    public void shapeCreated(String message, Session client) throws IOException, EncodeException {
+        for (Session otherSession : peers) {
+            if (!otherSession.equals(client)) {
+                _text = message;
+//                System.out.print("################# message = " + message);
+                otherSession.getBasicRemote().sendText(message);
+            }
+        }
+    }
+}
